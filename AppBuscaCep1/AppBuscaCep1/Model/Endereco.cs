7afcb4c6277d36bfccc3d5bb78b60c03511869ac
luare123 <luarbotelhoteste@gmail.com>
2023-02-14 14:04:07 -0300

    oi

---
+++ AppBuscaCep1/AppBuscaCep1/Model/Endereco.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppBuscaCep1.Model
+{
+    internal class Endereco
+    {
+    }
+}
---
+++ AppBuscaCep1/AppBuscaCep1/Service/DataService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppBuscaCep1.Service
+{
+    internal class DataService
+    {
+    }
+}
