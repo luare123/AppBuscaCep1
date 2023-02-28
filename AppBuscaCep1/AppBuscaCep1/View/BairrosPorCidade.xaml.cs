76243dc534c8ac46e72107b6a48c4afc551552e1
luare123 <luarbotelhoteste@gmail.com>
2023-02-28 14:02:17 -0300

    ultima atualizacao 28/02

--- AppBuscaCep1/AppBuscaCep1/View/BairrosPorCidade.xaml.cs
+++ AppBuscaCep1/AppBuscaCep1/View/BairrosPorCidade.xaml.cs
@@ -3,14 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace AppBuscaCep1.View
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class BairrosPorCidade : ContentView
+    public partial class BairrosPorCidade : ContentPage
     {
         public BairrosPorCidade()
         {
